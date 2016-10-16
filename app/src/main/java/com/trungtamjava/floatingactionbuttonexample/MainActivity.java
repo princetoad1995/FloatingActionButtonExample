cc764e1fbfc8834e0ed6b9f16b59d894ee4543e6
princetoad1995 <princetoad.thang@gmail.com>
2016-10-16 23:21:35 +0700

    First Commit

---
+++ app/src/main/java/com/trungtamjava/floatingactionbuttonexample/MainActivity.java
@@ -0,0 +1,26 @@
+package com.trungtamjava.floatingactionbuttonexample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Floating Actions Button", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
